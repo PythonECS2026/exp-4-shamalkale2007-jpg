d1b86bb4815a482cce2395ab3cb2b6b36d391dc5
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-05 22:32:09 +0530

    Fix print statement for simple interest output

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # AIM: Write a Python program to calculate the simple interest based on user input.
 # Coder:Shamal 
 # Date:26/01/26
+
 print("Simple Interest Calculator")
 principleAmount = float(input("Enter the principle amount: "))
 rateOfInterest = float(input("Enter the rate of interest: "))
 timePeriod = float(input("Enter the time period (in years): "))
 simple_interest = (principleAmount*rateOfInterest*timePeriod)/100
-print("Simple Interest:", simple_interest)print("Simple Interest Calculator")
+print("Simple Interest:", simple_interest)
