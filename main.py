b9507a1a8a58260b30869a90683e32a1322dddc9
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-05 22:38:37 +0530

    Update date format in main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 # AIM: Write a Python program to calculate the simple interest based on user input.
 # Coder:Shamal 
-# Date:26/01/26
-
-print("Simple Interest Calculator")
-principleAmount = float(input("Enter the principle amount: "))
-rateOfInterest = float(input("Enter the rate of interest: "))
-timePeriod = float(input("Enter the time period (in years): "))
-simple_interest = (principleAmount*rateOfInterest*timePeriod)/100
-print("Simple Interest:", simple_interest)
+# Date:26/01/2
