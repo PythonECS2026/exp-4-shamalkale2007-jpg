8697311667dd013e873e005932ff2b3efb663135
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-05 22:43:27 +0530

    Update comments and improve simple interest calculator

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# AIM: Write a Python program to calculate the simple interest based on user input.
-# Coder:Shamal 
-# Date:26/01/2
+#Aim: Calculating simple interest
+#Coder: Shamal
+#Date:26/01/26
+#Class:ECS/E2
+
+print("simple interest calculator")
+principal=float(input("Enter the principal amount:"))
+Rate=float(input("Enter the rate of interest:"))
+Time=float(input("Enter the time period(in years):"))
+SimpleInterest=(principal*Rate*Time)/100
+print("Simple Interest:", SimpleInterest)
