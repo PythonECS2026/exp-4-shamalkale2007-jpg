cacc9e1642c473541b94552b42c5e883b33f5cdd
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-22 10:55:26 +0530

    Refactor simple interest calculator variable names

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 #Date:26/01/26
 #Class:ECS/E2
 
-print("simple interest calculator")
-principal=float(input("Enter the principal amount:"))
-Rate=float(input("Enter the rate of interest:"))
-Time=float(input("Enter the time period(in years):"))
-SimpleInterest=(principal*Rate*Time)/100
-print("Simple Interest:", SimpleInterest)
+print("Simple Interest Calculator")
+principleAmount = float(input("Enter the principle amount: "))
+rateOfInterest = float(input("Enter the rate of interest: "))
+timePeriod = float(input("Enter the time period (in years): "))
+simple_interest = (principleAmount*rateOfInterest*timePeriod)/100
+print("Simple Interest:", simple_interest)
